ec8105de4b4c1c85ea94728bf93a27e400181347
Jose Ryan Pereira da Silva <jose.pereira@labs.dcx>
2023-06-01 11:24:41 -0300

    José Ryan

---
+++ JOAOVITOR.java
@@ -0,0 +1,11 @@
+
+
+
+public class JOAOVITOR {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+
+    }
+}
---
+++ joseryan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class joseryan {
+
+    Scanner leitor = new Scanner(System.in);
+
+    int A  = leitor.nextInt();
+    int B = leitor.nextInt();
+
+    public void resultado(){
+        int soma = A + B;
+    }
+    
+}
